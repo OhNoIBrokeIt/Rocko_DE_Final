f17543e3cc718cc92a2e1bbad13ef7102261ff36
OhNoIBrokeIt <williamcabrera@icloud.com>
2026-03-22 01:05:31 -0400

    feat: add starship medium prompt config

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,4 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
-end
-
-# Launch fastfetch on terminal open
-if status is-interactive
     fastfetch
 end
+starship init fish | source
